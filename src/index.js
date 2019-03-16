98ae764b262cdbd1e67b5cd9ba931a2f3463b831
Nura8 <bel_anna@list.ru>
2019-03-16 15:25:10 +0300

    solution puzzle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var arr = str.split("");
+
+    if (arr.lengt % 2 == 1) {
+        return false;
+    }
+
+    let i = 0;
+    while (arr.length != i + 1) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if ((arr[i] == bracketsConfig[j][0]) && (arr[i + 1] == bracketsConfig[j][1])) {
+                arr.splice(i, 2);
+                i = i - 2;
+                if (arr.length == 0) {
+                    return true;
+                }
+                break;
+            }
+        }
+        i++;
+    }
+    return false;
 }
